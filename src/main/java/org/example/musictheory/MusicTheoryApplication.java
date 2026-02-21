673e17316610c200d932b1bcf6ffe954be636b56
Aleqzander Baker <aleqzanderbaker@gmail.com>
2026-02-20 18:33:56 -0700

    added basic template for static and template html to interact with java endpoints

---
+++ src/main/java/org/example/musictheory/MusicTheoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.musictheory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicTheoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicTheoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/musictheory/MusicTheoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.musictheory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicTheoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
